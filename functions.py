81b7165708d40b1d17502b519ddbbdcfd921da4d
Artem Radchenko <sdad@bk.ru>
2024-04-03 01:51:57 +0300

    Вспомогательные функции. Исправленные и проверенные.

--- functions.py
+++ functions.py
@@ -1,17 +1,70 @@
 # Функции телефонного справочника
 import json
 
-# Функция для загрузки данных из файла
-def load_data(file_name):
+# Загрузка данных телефонной книги из файла phone_book.json
+def load_phone_book():
+    try:
+        with open('phone_book.json', 'r', encoding='utf-8') as file:
+            phone_book = json.load(file)
+    except FileNotFoundError:
+        phone_book = {}
+    return phone_book
+
+# Сохранение данных телефонной книги в файл phone_book.json
+def save_phone_book(phone_book):
+    with open('phone_book.json', 'w', encoding='utf-8') as file:
+        json.dump(phone_book, file, ensure_ascii=False, indent=4)
+
+# Импорт данных телефонной книги из указанного файла
+def import_phone_book(file_name):
     try:
         with open(file_name, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-        return data
+            imported_data = json.load(file)
+        save_phone_book(imported_data)
+        print("Данные успешно импортированы.")
     except FileNotFoundError:
-        return {}
+        print("Указанный файл не найден.")
 
-# Функция для сохранения данных в файл
-def save_data(file_name, data):
-    with open(file_name, 'w', encoding='utf-8') as file:
-        json.dump(data, file, ensure_ascii=False)
+# Добавление нового контакта в телефонную книгу
+def add_contact(name, number, email):
+    phone_book = load_phone_book()
+    phone_book[name] = {'номер': number, 'email': email}
+    save_phone_book(phone_book)
 
+# Просмотр контактов в телефонной книге
+def view_contacts():
+    phone_book = load_phone_book()
+    if phone_book:
+        for name, contact in phone_book.items():
+            print(f'Имя: {name}, Номер: {contact["номер"]}, Email: {contact["email"]}')
+    else:
+        print('Телефонная книга пуста.')
+
+# Поиск контакта
+def search_contact(name):
+    phone_book = load_phone_book()
+    if name in phone_book:
+        contact = phone_book[name]
+        print(f'Имя: {name}, Номер: {contact["номер"]}, Email: {contact["email"]}')
+    else:
+        print(f'Контакт {name} не найден.')
+
+# Удаление контакта
+def delete_contact(name):
+    phone_book = load_phone_book()
+    if name in phone_book:
+        del phone_book[name]
+        save_phone_book(phone_book)
+        print(f'Контакт {name} удалён.')
+    else:
+        print(f'Контакт {name} не найден.')
+
+# Обновление контакта
+def update_contact(name, number, email):
+    phone_book = load_phone_book()
+    if name in phone_book:
+        phone_book[name] = {'номер': number, 'email': email}
+        save_phone_book(phone_book)
+        print(f'Контакт {name} изменен.')
+    else:
+        print(f'Контакт {name} не найден.')
