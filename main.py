d6fe3473b9ab169e811a0b786055e4fbef3c8ebc
Artem Radchenko <sdad@bk.ru>
2024-04-03 01:47:10 +0300

    Основная функция. Исправленная и проверенная.

--- main.py
+++ main.py
@@ -1,50 +1,44 @@
 # Основная функция программы
-from functions import *
+from functions import add_contact, view_contacts, search_contact, delete_contact, update_contact, import_phone_book
 
 def main():
-    file_name = 'phone_book.json'
-    data = load_data(file_name)
-
     while True:
-        print('\nМеню Телефонного справочника:')
-        print('1. Просмотр всех записей')
-        print('2. Добавить новую запись')
-        print('3. Поиск записи')
-        print('4. Удаление записи')
-        print('5. Обновление записи')
-        print('6. Импорт данных')
-        print('7. Выход')
+        print("\nМеню Телефонного справочника:")
+        print("1. Просмотр контактов")
+        print("2. Добавить контакт")
+        print("3. Поиск контакта")
+        print("4. Удалить контакт")
+        print("5. Обновить контакт")
+        print("6. Импорт данных из файла")
+        print("7. Выход")
 
-        choice = input('Введите ваш выбор: ')
+        choice = input("Введите ваш выбор: ")
 
         if choice == '1':
-            view_records(data)
+            view_contacts()
         elif choice == '2':
-            name = input('Введите имя: ')
-            number = input('Введите номер: ')
-            add_record(data, name, number)
+            name = input("Введите имя: ")
+            number = input("Введите номер: ")
+            email = input("Введите email: ")
+            add_contact(name, number, email)
         elif choice == '3':
-            name = input('Введите имя для поиска: ')
-            search_record(data, name)
+            name = input("Введите имя для поиска: ")
+            search_contact(name)
         elif choice == '4':
-            name = input('Введите имя для удаления: ')
-            delete_record(data, name)
+            name = input("Введите имя для удаления: ")
+            delete_contact(name)
         elif choice == '5':
-            name = input('Введите имя для обновления: ')
-            new_number = input('Введите новый номер: ')
-            update_record(data, name, new_number)
+            name = input("Введите имя для обновления: ")
+            number = input("Введите новый номер: ")
+            email = input("Введите новый email: ")
+            update_contact(name, number, email)
         elif choice == '6':
-            file_to_import = input('Введите имя файла для импорта: ')
-            imported_data = import_data(file_to_import)
-            if imported_data:
-                data.extend(imported_data)
-                save_data(file_name, data)
-                print('Данные успешно импортированы.')
+            file_name = input("Введите имя файла для импорта: ")
+            import_phone_book(file_name)
         elif choice == '7':
-            print('Выход из Телефонного справочника.')
             break
         else:
-            print('Неверный выбор. Пожалуйста, попробуйте снова.')
+            print("Неверный выбор. Попробуйте снова.")
 
 if __name__ == "__main__":
     main()
